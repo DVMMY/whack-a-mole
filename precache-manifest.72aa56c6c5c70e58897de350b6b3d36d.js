a40d0e87592c5cf0dc32b91c823f413bb820d74e
Dammy <damols-adeniyi@hotmail.co.uk>
2020-09-01 23:14:58 +0100

    Updates

---
+++ precache-manifest.72aa56c6c5c70e58897de350b6b3d36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf99c21675c9a152949dd93056f1f88",
+    "url": "/whack-a-mole/index.html"
+  },
+  {
+    "revision": "bcb2bb542a0f4d6fe0a4",
+    "url": "/whack-a-mole/static/css/main.9fb4acdf.chunk.css"
+  },
+  {
+    "revision": "738d5347ff8c2e9dc0c0",
+    "url": "/whack-a-mole/static/js/2.4ab19517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whack-a-mole/static/js/2.4ab19517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb2bb542a0f4d6fe0a4",
+    "url": "/whack-a-mole/static/js/main.8823e296.chunk.js"
+  },
+  {
+    "revision": "56d0cd21bbe97053521e",
+    "url": "/whack-a-mole/static/js/runtime-main.6c374291.js"
+  }
+]);
